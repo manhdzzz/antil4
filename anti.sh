e47ca8c9f806dd2da81336d6526412363f650baa
menjmoi <101183159+manhdzzz@users.noreply.github.com>
2024-08-17 01:04:37 +0700

    Update anti.sh

--- anti.sh
+++ anti.sh
@@ -1,14 +1,16 @@
+# Anti ddos tcp, syn flood, only allow 80 443 22 and VN
+# MENJMOI COPYRIGHT DO NOT REUP
 
-# Anti ddos tcp by menjmoi
 #!/bin/bash
 
 IP_LIST="vn.txt"
+BLACKLIST="blacklist"
 
 # Xóa tất cả các quy tắc hiện tại
 iptables -F
 iptables -X
 
-# Đặt chính sách mặc định: Chặn tất cả các kết nối
+# Đặt chính sách mặc định
 iptables -P INPUT DROP
 iptables -P FORWARD DROP
 iptables -P OUTPUT ACCEPT
@@ -27,15 +29,13 @@ iptables -A INPUT -i lo -j ACCEPT
 # Cho phép các kết nối đã thiết lập hoặc liên quan
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
-# Bảo vệ chống SYN Flood: Giới hạn số lượng gói SYN từ cùng một IP
-iptables -A INPUT -p tcp --syn -m limit --limit 2/s --limit-burst 4 -j ACCEPT
-iptables -A INPUT -p tcp --syn -m recent --name synflood --set
-iptables -A INPUT -p tcp --syn -m recent --name synflood --update --seconds 60 --hitcount 20 -j BLACKLIST
+# Bảo vệ chống SYN Flood
+iptables -A INPUT -p tcp --syn -m limit --limit 10/s --limit-burst 20 -j ACCEPT
 
 # Giới hạn kết nối mới: chỉ chấp nhận tối đa 10 kết nối mới từ cùng một IP mỗi phút
-iptables -A INPUT -p tcp --syn -m connlimit --connlimit-above 3 --connlimit-mask 32 -j BLACKLIST
+iptables -A INPUT -p tcp --syn -m connlimit --connlimit-above 10 --connlimit-mask 32 -j BLACKLIST
 
-# Giới hạn kích thước gói tin (0 đến 100 byte) cho HTTP và HTTPS
+# Giới hạn kích thước gói tin (0 đến 100 byte)
 iptables -A INPUT -p tcp --dport 80 -m length --length 0:100 -j ACCEPT
 iptables -A INPUT -p tcp --dport 443 -m length --length 0:100 -j ACCEPT
 
@@ -46,12 +46,12 @@ do
     iptables -A INPUT -s $ip_range -p tcp -m multiport --dports 80,443 -j ACCEPT
 done < "$IP_LIST"
 
-# Chặn tất cả các kết nối không thuộc IP trong vn.txt (dạng blacklist)
-iptables -A INPUT -p tcp -m multiport --dports 80,443 -j BLACKLIST
-
 # Kiểm tra các kết nối không hợp lệ và block 1 phút nếu không hợp lệ
 iptables -A INPUT -p tcp -m length --length 101: -j BLACKLIST
 iptables -A INPUT -p tcp -m state --state NEW -m recent --set --name blacklist --rsource
 iptables -A INPUT -m recent --update --seconds 60 --name blacklist --rsource -j DROP
+
+# Chặn tất cả các kết nối không phải từ IP được cho phép
+iptables -A INPUT -j BLACKLIST
 
 echo "All rules applied. Blocking all other connections."
